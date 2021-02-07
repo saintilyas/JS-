ae18d28655cb284e3c838492d8f140332f8d80f3
saintilyas <78703057+saintilyas@users.noreply.github.com>
2021-02-07 23:21:17 +0300

    created

---
+++ ghbd.js
@@ -0,0 +1,9 @@
+document.querySelector('.kakashka').addEventListener('click',function() 
+{
+this.classList.toggle("night");
+});
+
+document.querySelector('.ssylo4ka').addEventListener('click',function() 
+{
+window.location.href = "https://www.kinopoisk.ru/film/694051/";
+});
